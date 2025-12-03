d1a68a5f7d00eb0a62bee8d9a171f9103b45982f
Shifani <shifani@cognizant.com>
2025-12-03 21:46:30 +0530

    added background image

--- frontend/src/pages/CreateProjectPage.tsx
+++ frontend/src/pages/CreateProjectPage.tsx
@@ -377,7 +377,15 @@ export default function CreateProjectPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    
+    <div
+  className="min-h-screen py-8 bg-cover bg-center bg-no-repeat relative"
+  style={{
+    backgroundImage:
+      "linear-gradient(to bottom right, rgba(255,255,255,0.85), rgba(255,255,255,0.95)), url('/images/background1.jpeg')",
+  }}
+>
+
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="mb-8">
